2e88334362d03ecad621b2d9f332fd96ab372d50
Michael Franzl <public.michael@franzl.name>
2020-03-22 10:07:06 +0100

    Introduce code style using eslint

--- src/captain-hook.js
+++ src/captain-hook.js
@@ -1,66 +1,37 @@
-/*jshint esversion: 6 */
-
-/*
-captain-hook
-
-A configurable mix-in providing flexible event emission for JavaScript objects.
-
-Copyright 2017 Michael Karl Franzl
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-
-/** 
+/**
  * Callback function consuming an event.
- * 
+ *
  * @callback {Function} EventHandler
  * @param {...*} args - Arguments provided by the emitter of the event ({@link EmitEvent}).
  * @returns {*} Optionally returned values of attached EventHandlers
  * are concatenated into an Array and passed back to the {@link EmitEvent}.
  */
 
-
-
 /**
 * Object implementing event emitter behavior which can be mixed into other
 * objects, classes, or prototypes. See README for illustrations.
-* 
+*
 * @typedef {Object} EventEmitter
-* @property {AddEventHandler} on Associates an event handler function
+* @property {AddEventHandler} on - Associates an event handler function
 * with an event name.
-* @property {AddOneTimeEventHandler} once Same as {@link AddEventHandler}, but sets `options.once` to `true`.
-* @property {RemoveEventHandler} off Remove an event handler.
-* @property {EmitEvent} _emit Call all registered event
+* @property {AddOneTimeEventHandler} once - Same as {@link AddEventHandler}, but sets `options.once`
+* to `true`.
+* @property {RemoveEventHandler} off - Remove an event handler.
+* @property {EmitEvent} _emit - Call all registered event
 * handlers.
 */
 
 
 /**
- * Factory returning a plain object implementing event emission behavior.
- * 
+ * Factory function which returns a plain object which implements event emission behavior.
+ *
  * The default method/property names for adding and removing handlers are
  * `on()`, `off()`, and `_emit()`. The attached event handler functions are
  * stored in a property named `_handlers`.
- * 
+ *
  * All property/method names can be explicitly configured via `config` to
  * prevent naming conflicts with existing propety names of the target object.
- * 
+ *
  * @example
  * let event_emitter = CaptainHook({
  *   on_prop:       'on',       // public
@@ -68,7 +39,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *   emit_prop:     '_emit',    // pseudo-private
  *   handlers_prop: '_handlers' // pseudo-private
  * });
- * 
+ *
  * @param {Object} [config={}] Property names of the returned object.
  * @param {String} [config.on_prop=on] Method property name for setting event
  * handlers.
@@ -82,21 +53,20 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * otherwise event handlers would be shared across instances.
  * @returns {EventEmitter}
  */
-var CaptainHook = function ({
-  on_prop        = 'on',
-  once_prop      = 'once',
-  off_prop       = 'off',
-  emit_prop      = '_emit',
-  handlers_prop  = '_handlers',
+function CaptainHook({
+  on_prop = 'on',
+  once_prop = 'once',
+  off_prop = 'off',
+  emit_prop = '_emit',
+  handlers_prop = '_handlers',
 } = {}) {
-  
   let privatehandlers;
-  if (handlers_prop == null) {
-    // Use a truly private storage for event handlers instead of a
-    // publicly accessible one.
-    privatehandlers = {};
-  }
-  
+
+  const handlersPropName = handlers_prop;
+
+  // Use a truly private storage for event handlers instead of a publicly accessible one.
+  if (handlersPropName == null) privatehandlers = {};
+
   return {
     /**
     * Associates an event handler function with an event name.
@@ -115,9 +85,9 @@ var CaptainHook = function ({
     * @param {Boolean} [options.once=false] - When `true` then `handler` runs only
     * once, then is removed.
     */
-    [on_prop] (eventname, callable, options={}) {
-      let handler = {
-        callable: callable,
+    [on_prop](eventname, callable, options = {}) {
+      const handler = {
+        callable,
         tag: options.tag,
         priority: options.priority || 10,
         context: options.context || this,
@@ -126,128 +96,111 @@ var CaptainHook = function ({
 
       // Choose and initialize the handlers storage
       let handlers;
-      if (handlers_prop) {
-        if (!this[handlers_prop]) {
-          // Only ran once.
-          // `this` will be the instance, not the prototype.
-          this[handlers_prop] = {};
-        }
-        handlers = this[handlers_prop];
+      if (handlersPropName) {
+        if (!this[handlersPropName]) this[handlersPropName] = {}
+        handlers = this[handlersPropName];
       } else {
-        // Use the privately scoped storage instead.
         handlers = privatehandlers;
       }
-      
+
       if (!handlers[eventname]) {
         handlers[eventname] = [handler];
       } else {
         handlers[eventname].push(handler);
       }
-      handlers[eventname].sort(function(a, b) {
-        return b.priority - a.priority;
-      });
+      handlers[eventname].sort((a, b) => (b.priority - a.priority));
     },
-    
-    
+
+
     /**
     * Same as {@link AddEventHandler}, but sets `options.once` to `true`.
     *
     * @function AddOneTimeEventHandler
     * @alias AddEventHandler
     */
-    [once_prop] (eventname, callable, options={}) {
-      options.once = true;
-      this[on_prop](eventname, callable, options);
+    [once_prop](eventname, callable, options = {}) {
+      const opts = options;
+      opts.once = true;
+      this[on_prop](eventname, callable, opts);
     },
-    
-    
-    /** 
+
+
+    /**
      * Removes an event handler tagged with `tag` from `eventname`. If `tag` or
      * `eventname` are not registered, this method does nothing.
-     * 
+     *
      * @function RemoveEventHandler
      * @param {String} eventname
      * @param {String} tag
      */
-    [off_prop] (eventname, tag) {
+    [off_prop](eventname, tag) {
       // Choose the handlers storage.
       let handlers;
-      if (handlers_prop) {
-        handlers = this[handlers_prop];
+      if (handlersPropName) {
+        handlers = this[handlersPropName];
       } else {
         handlers = privatehandlers;
       }
-      
-      if (!tag || !handlers || !handlers[eventname]) {
-        // Nothing to do.
-        return;
-      }
-      
-      let eventhandlers = handlers[eventname];
-      
+
+      if (!tag || !handlers || !handlers[eventname]) return;
+
+      const eventhandlers = handlers[eventname];
+
       // Find the event handler with matching tag in the array.
-      let i = 0;
+      let i;
       let found = false;
-      for (let eventhandler of eventhandlers) {
-        if (eventhandler.tag == tag) {
+      for (i = 0; i < eventhandlers.length; i += 1) {
+        const eventhandler = eventhandlers[i];
+        if (eventhandler.tag === tag) {
           found = true;
           break;
         }
-        i++;
       }
-      if (found) {
-        // Remove this handler
-        eventhandlers.splice(i, 1);
-      }
+      if (found) eventhandlers.splice(i, 1); // Remove this handler
     },
-    
-    
+
+
     /**
     * Calls all registered event handler callbacks with the provided arguments.
-    * 
+    *
     * Note that return values of event handlers are not visible to
     * any other event handler, which provides isolation between
     * event handlers as well as privacy.
-    * 
+    *
     * To implement content filtering, you need to pass the content
     * by reference (i.e. to filter strings, you need to wrap the
     * string into an object or array).
-    * 
+    *
     * @function EmitEvent
     * @param {String} eventname
     * @param {...*} args - Arguments to call the handler with.
     * @returns {Array} - Each return value of handlers concatenated.
     */
-    [emit_prop] (eventname, ...args) {
+    [emit_prop](eventname, ...args) {
       // Choose the handlers storage.
       let handlers;
-      if (handlers_prop) {
-        handlers = this[handlers_prop];
+      if (handlersPropName) {
+        handlers = this[handlersPropName];
       } else {
         handlers = privatehandlers;
       }
-      
-      let retvals = [];
-      if (!handlers || !handlers[eventname]) {
-        // Nothing to do
-        return retvals;
-      }
-      
-      for (let eventhandler of handlers[eventname]) {
+
+      const retvals = [];
+      if (!handlers || !handlers[eventname]) return retvals;
+
+      handlers[eventname].forEach((eventhandler) => {
         retvals.push(eventhandler.callable.call(eventhandler.context, ...args));
-      }
-      
+      });
+
       // remove one-time event handlers
-      for (let i = handlers[eventname].length - 1; i >= 0; i--) {
-        let handler = handlers[eventname][i];
-        if (handler.once) {
-          handlers[eventname].splice(i, 1);
-        }
+      for (let i = handlers[eventname].length - 1; i >= 0; i -= 1) {
+        const handler = handlers[eventname][i];
+        if (handler.once) handlers[eventname].splice(i, 1);
       }
-      
+
       return retvals;
     },
-  }; // return
-};
+  };
+}
 
 export default CaptainHook;
--- tests/test.js
+++ tests/test.js
@@ -1,106 +1,106 @@
-/*jshint esversion: 6 */
+/* eslint-disable prefer-arrow-callback, func-names */
 
-const CaptainHook = require('../dist/captain-hook.umd.min.js');
+import assert from 'assert';
 
-const assert = require('assert');
+import CaptainHook from '../src/captain-hook.js'
 
-it('sets default method name `on` in prototype', function() {
-  let inst = Object.create(CaptainHook());
+it('sets default method name `on` in prototype', function () {
+  const inst = Object.create(CaptainHook());
   assert.equal(inst.hasOwnProperty('on'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('on'), true);
 });
 
-it('sets default method name `once` in prototype', function() {
-  let inst = Object.create(CaptainHook());
+it('sets default method name `once` in prototype', function () {
+  const inst = Object.create(CaptainHook());
   assert.equal(inst.hasOwnProperty('once'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('once'), true);
 });
 
-it('sets default method name `off` in prototype', function() {
-  let inst = Object.create(CaptainHook());
+it('sets default method name `off` in prototype', function () {
+  const inst = Object.create(CaptainHook());
   assert.equal(inst.hasOwnProperty('off'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('off'), true);
 });
-  
-it('sets default method name `_emit` in prototype', function() {
-  let inst = Object.create(CaptainHook());
+
+it('sets default method name `_emit` in prototype', function () {
+  const inst = Object.create(CaptainHook());
   assert.equal(inst.hasOwnProperty('_emit'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('_emit'), true);
 });
-  
-it('sets default property name `_handlers` on instance', function() {
-  let inst = Object.create(CaptainHook());
-  inst.on('event', function() {});
+
+it('sets default property name `_handlers` on instance', function () {
+  const inst = Object.create(CaptainHook());
+  inst.on('event', function () {});
   assert.equal(inst.hasOwnProperty('_handlers'), true);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('_handlers'), false);
 });
 
-it('cannot access truly private handler storage', function() {
-  let inst = Object.create(CaptainHook({
+it('cannot access truly private handler storage', function () {
+  const inst = Object.create(CaptainHook({
     handlers_prop: null,
   }));
-  inst.on('event', function() {}); // this creates _handlers on instance
+  inst.on('event', function () {}); // this creates _handlers on instance
   assert.equal(inst.hasOwnProperty('_handlers'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('_handlers'), false);
 });
 
 
-it('sets custom method name for `on`', function() {
-  let inst = Object.create(CaptainHook({
+it('sets custom method name for `on`', function () {
+  const inst = Object.create(CaptainHook({
     on_prop: 'myon'
   }));
   assert.equal(inst.hasOwnProperty('myon'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('myon'), true);
 });
 
-it('sets custom method name for `once`', function() {
-  let inst = Object.create(CaptainHook({
+it('sets custom method name for `once`', function () {
+  const inst = Object.create(CaptainHook({
     once_prop: 'myonce'
   }));
   assert.equal(inst.hasOwnProperty('myonce'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('myonce'), true);
 });
 
-it('sets custom method name for `off`', function() {
-  let inst = Object.create(CaptainHook({
+it('sets custom method name for `off`', function () {
+  const inst = Object.create(CaptainHook({
     off_prop: 'myoff'
   }));
   assert.equal(inst.hasOwnProperty('myoff'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('myoff'), true);
 });
 
-it('sets custom method name for `_emit`', function() {
-  let inst = Object.create(CaptainHook({
+it('sets custom method name for `_emit`', function () {
+  const inst = Object.create(CaptainHook({
     emit_prop: 'myemit'
   }));
   assert.equal(inst.hasOwnProperty('myemit'), false);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('myemit'), true);
 });
 
-it('sets custom method name for `_handlers`', function() {
-  let inst = Object.create(CaptainHook({
+it('sets custom method name for `_handlers`', function () {
+  const inst = Object.create(CaptainHook({
     handlers_prop: '_myhandlers'
   }));
-  inst.on('event', function() {}); // this creates _handlers on instance
+  inst.on('event', function () {}); // this creates _handlers on instance
   assert.equal(inst.hasOwnProperty('_myhandlers'), true);
   assert.equal(Object.getPrototypeOf(inst).hasOwnProperty('_myhandlers'), false);
 });
 
 
-it('repeatedly calls registered handlers, concatenates the return values, and sorts callbacks by priority', function() {
-  let App = function() {
-    this.do = function() {
-      let results = this._emit('some_event');
+it('repeatedly calls registered handlers, concatenates the return values, and sorts callbacks by priority', function () {
+  const App = function () {
+    this.do = function () {
+      const results = this._emit('some_event');
       assert.equal(results[0], 'b');
       assert.equal(results[1], 'a');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  let inst = new App();
-  inst.on('some_event', function() { return 'a'; }, {
+  const inst = new App();
+  inst.on('some_event', function () { return 'a'; }, {
     priority: 2,
   });
-  inst.on('some_event', function() { return 'b'; }, {
+  inst.on('some_event', function () { return 'b'; }, {
     priority: 9,
   });
   inst.do();
@@ -108,10 +108,10 @@ it('repeatedly calls registered handlers, concatenates the return values, and so
 });
 
 
-it('uses truly private callback storage', function() {
-  let App = function() {
-    this.do = function() {
-      let results = this._emit('some_event');
+it('uses truly private callback storage', function () {
+  const App = function () {
+    this.do = function () {
+      const results = this._emit('some_event');
       assert.equal(results[0], 'b');
       assert.equal(results[1], 'a');
     };
@@ -119,129 +119,129 @@ it('uses truly private callback storage', function() {
   Object.assign(App.prototype, CaptainHook({
     handlers_prop: null,
   }));
-  let inst = new App();
-  inst.on('some_event', function() { return 'a'; }, {
+  const inst = new App();
+  inst.on('some_event', function () { return 'a'; }, {
     priority: 2,
   });
-  inst.on('some_event', function() { return 'b'; }, {
+  inst.on('some_event', function () { return 'b'; }, {
     priority: 9,
   });
   inst.do();
 });
 
 
-it('calls registered handlers only once', function() {
-  let App = function() {
-    this.do = function() {
+it('calls registered handlers only once', function () {
+  const App = function () {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  let inst = new App();
-  inst.once('some_event', function() { return 'a'; });
+  const inst = new App();
+  inst.once('some_event', function () { return 'a'; });
   let result = inst.do();
   assert.equal(result[0], 'a');
-  
+
   result = inst.do(); // second time should not call event handler
   assert.equal(result.length, 0);
 });
 
 
-it('removes registered handlers by tag', function() {
-  let App = function() {
-    this.do = function() {
+it('removes registered handlers by tag', function () {
+  const App = function () {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  let inst = new App();
-  inst.on('some_event', function() { return 'a'; }, {
-    tag: 'mysecret'
+  const inst = new App();
+  inst.on('some_event', function () { return 'a'; }, {
+    tag: 'mysecret',
   });
   let result = inst.do();
   assert.equal(result[0], 'a');
-  
+
   inst.off('some_event', 'mysecret');
   result = inst.do(); // second time should not call event handler
   assert.equal(result.length, 0);
 });
 
 
-it('does not remove registered handlers when supplying wrong tag', function() {
-  let App = function() {
-    this.do = function() {
-      let results = this._emit('some_event');
+it('does not remove registered handlers when supplying wrong tag', function () {
+  const App = function () {
+    this.do = function () {
+      const results = this._emit('some_event');
       assert.equal(results[0], 'a');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  let inst = new App();
-  inst.on('some_event', function() { return 'a'; }, {
-    tag: 'mysecret'
+  const inst = new App();
+  inst.on('some_event', function () { return 'a'; }, {
+    tag: 'mysecret',
   });
   inst.do();
-  
+
   inst.off('some_event', 'forgotsecret');
   inst.do(); // second time should succeed too
 });
 
 
-it('does not share handler storage between instances', function() {
-  let App = function() {
-    this.do = function() {
+it('does not share handler storage between instances', function () {
+  const App = function () {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  
-  let inst1 = new App();
-  inst1.on('some_event', function() { return 'a'; }, {
+
+  const inst1 = new App();
+  inst1.on('some_event', function () { return 'a'; }, {
     priority: 999,
   });
-  inst1.on('some_event', function() { return 'b'; }, {
+  inst1.on('some_event', function () { return 'b'; }, {
     priority: 1001,
   });
-  
-  let inst2 = new App();
-  inst2.on('some_event', function() { return 'c'; }, {
+
+  const inst2 = new App();
+  inst2.on('some_event', function () { return 'c'; }, {
     priority: 2,
   });
-  inst2.on('some_event', function() { return 'd'; }, {
+  inst2.on('some_event', function () { return 'd'; }, {
     priority: 9,
   });
-  let result = inst2.do();
+  const result = inst2.do();
   assert.equal(result[0], 'd');
   assert.equal(result[1], 'c');
 });
 
 
-it('does share handler storage between instances when using private storage, this is a feature and a \'gotcha\'', function() {
-  let App = function() {
-    this.do = function() {
+it('does share handler storage between instances when using private storage, this is a feature and a \'gotcha\'', function () {
+  const App = function () {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
   Object.assign(App.prototype, CaptainHook({
     handlers_prop: null,
   }));
-  
-  let inst1 = new App();
-  inst1.on('some_event', function() { return 'a'; }, {
+
+  const inst1 = new App();
+  inst1.on('some_event', function () { return 'a'; }, {
     priority: 4,
   });
-  inst1.on('some_event', function() { return 'b'; }, {
+  inst1.on('some_event', function () { return 'b'; }, {
     priority: 3,
   });
-  
-  let inst2 = new App();
-  inst2.on('some_event', function() { return 'c'; }, {
+
+  const inst2 = new App();
+  inst2.on('some_event', function () { return 'c'; }, {
     priority: 2,
   });
-  inst2.on('some_event', function() { return 'd'; }, {
+  inst2.on('some_event', function () { return 'd'; }, {
     priority: 1,
   });
-  let result = inst2.do();
-  
+  const result = inst2.do();
+
   assert.equal(result[0], 'a');
   assert.equal(result[1], 'b');
   assert.equal(result[2], 'c');
@@ -249,51 +249,51 @@ it('does share handler storage between instances when using private storage, thi
 });
 
 
-it('does not share handler storage between instances when mixed into instances and when using private handler storage ', function() {
-  let App = function() {
-    this.do = function() {
+it('does not share handler storage between instances when mixed into instances and when using private handler storage ', function () {
+  const App = function () {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
-  
-  let inst1 = new App();
+
+  const inst1 = new App();
   Object.assign(inst1, CaptainHook({
     handlers_prop: null,
   }));
-  
-  let inst2 = new App();
+
+  const inst2 = new App();
   Object.assign(inst2, CaptainHook({
     handlers_prop: null,
   }));
-  
-  inst1.on('some_event', function() { return 'a'; }, {
+
+  inst1.on('some_event', function () { return 'a'; }, {
     priority: 999,
   });
-  inst1.on('some_event', function() { return 'b'; }, {
+  inst1.on('some_event', function () { return 'b'; }, {
     priority: 1001,
   });
 
-  inst2.on('some_event', function() { return 'c'; }, {
+  inst2.on('some_event', function () { return 'c'; }, {
     priority: 2,
   });
-  inst2.on('some_event', function() { return 'd'; }, {
+  inst2.on('some_event', function () { return 'd'; }, {
     priority: 9,
   });
-  let result = inst2.do();
+  const result = inst2.do();
   assert.equal(result[0], 'd');
   assert.equal(result[1], 'c');
 });
 
 
-it('passes arguments to event handlers', function() {
-  let App = function() {
-    this.do = function() {
-      let results = this._emit('some_event', 1, 'two', {three: 3});
+it('passes arguments to event handlers', function () {
+  const App = function () {
+    this.do = function () {
+      const results = this._emit('some_event', 1, 'two', {three: 3});
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  
-  let inst = new App();
+
+  const inst = new App();
   inst.on('some_event', function(num, str, obj) {
     assert.equal(num, 1);
     assert.equal(str, 'two');
@@ -303,38 +303,38 @@ it('passes arguments to event handlers', function() {
 });
 
 
-it('sets `this` context of event handlers, by default to the instance', function() {
-  let App = function() {
+it('sets `this` context of event handlers, by default to the instance', function () {
+  const App = function () {
     this.string = 'hello';
-    this.do = function() {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  
-  let inst = new App();
-  inst.on('some_event', function() {
+
+  const inst = new App();
+  inst.on('some_event', function () {
     return this.string;
   });
   assert.equal(inst.do(), 'hello');
 });
 
 
-it('sets `this` context of event handlers to some other object', function() {
-  let App = function() {
+it('sets `this` context of event handlers to some other object', function () {
+  const App = function () {
     this.string = 'hello';
-    this.do = function() {
+    this.do = function () {
       return this._emit('some_event');
     };
   };
   Object.assign(App.prototype, CaptainHook());
-  
-  let contextobj = {
+
+  const contextobj = {
     string: 'world',
   };
-  
-  let inst = new App();
-  inst.on('some_event', function() {
+
+  const inst = new App();
+  inst.on('some_event', function () {
     return this.string;
   }, {
     context: contextobj,
